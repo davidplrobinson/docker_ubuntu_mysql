f9ea0c7fa847bbf29b96e726c7f94c7c66cd1654
David Robinson <david.pl.robinson@googlemail.com>
2017-07-07 11:11:00 +1000

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Docker Ubuntu Base with MySQL
---
+++ docker_install.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+# Tested and working on ubuntu. 
+
+apt-get update
+apt-get install -y --no-install-recommends linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+apt-get update
+#apt-get -y install docker-engine
+apt-get -y install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+docker run hello-world
